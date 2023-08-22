70a3ba199740005782fc80628fc5ecec92ccbcd7
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-08-22 15:47:36 +0300

    counting

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.strip()[-1] in [".", "?", "!"]
+
+    def is_question(self):
+        return self.value.strip()[-1] == "?"
+
+    def is_exclamation(self):
+        return self.value.strip()[-1] == "!"
+
+    def count_sentences(self):
+        sentences = self.value.split(".")
+        sentences = [s for s in sentences if s]
+        return len(sentences)
+
